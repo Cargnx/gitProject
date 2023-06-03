adea6e23da11f4e948a107feac629e9f24a425b8
Cargnx <lemi25yuerekli@gmail.com>
2023-06-03 16:43:35 +0200

    First GIT commit with c++

---
+++ HelloWorld.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello World!" << endl;
+
+    int a = 10;
+    if (a == 10)
+    {
+        cout << "a is 10" << endl;
+    }
+    else
+    {
+        cout << "a is not 10" << endl;
+    }
+
+    return 0;
+}
